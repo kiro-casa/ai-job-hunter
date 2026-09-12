bd26425a908871a679ed0e8f7e69c4f8123d0faf
Casa King Xyro <kingxyrocasa@gmail.com>
2026-09-13 02:22:04 +0800

    Fix web.php catch-all route for React

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Catch-all route to serve the React frontend
+Route::get('/{any}', function () {
+    $indexPath = public_path('index.html');
+    
+    if (file_exists($indexPath)) {
+        return file_get_contents($indexPath);
+    }
+    
+    return response()->view('errors.500', [
+        'message' => 'Frontend build (index.html) is missing from the public folder.'
+    ], 500);
+})->where('any', '.*');
