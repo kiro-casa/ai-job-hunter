acae60581f2dca7fb8bda051dfba7c847b636eff
Casa King Xyro <kingxyrocasa@gmail.com>
2026-09-13 09:51:16 +0800

    Add request logging to debug API issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,6 +13,8 @@ if (!apiURL) {
   }
 }
 
+console.log('API URL detected:', apiURL);
+
 const api = axios.create({
   baseURL: apiURL,
   headers: {
@@ -22,5 +24,26 @@ const api = axios.create({
   withCredentials: true, 
   withXSRFToken: true,   
 });
+
+// Log all requests
+api.interceptors.request.use(config => {
+  console.log('Request:', config.method?.toUpperCase(), config.url);
+  return config;
+}, error => {
+  console.error('Request error:', error);
+  return Promise.reject(error);
+});
+
+// Log all responses
+api.interceptors.response.use(
+  response => {
+    console.log('Response OK:', response.status, response.config.url);
+    return response;
+  },
+  error => {
+    console.error('Response error:', error.response?.status, error.config?.url, error.response?.data);
+    return Promise.reject(error);
+  }
+);
 
 export default api;
